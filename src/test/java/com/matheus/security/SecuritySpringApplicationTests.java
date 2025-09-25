e6a09713a1b327c2e15c24fcd2bf9cbca4133093
Matheus Maranhão <matheus2018conta@gmail.com>
2025-09-25 14:54:10 -0300

    initial commit

---
+++ src/test/java/com/matheus/security/SecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
